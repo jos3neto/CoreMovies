fba0b077dcccd0d7d072cb02dcad631c626305fc
jos3neto <jose.neto@gmx.com>
2019-05-20 21:56:08 -0300

    Initial Commit

---
+++ CoreMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMovies
+//
+//  Created by Jose on 20/5/19.
+//  Copyright © 2019 appcat.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
