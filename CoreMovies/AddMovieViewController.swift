c21e118ac784a103f976222f2827c86506ce6f0e
jos3neto <jose.neto@gmx.com>
2019-05-22 17:18:09 -0300

    tableView working fine.

---
+++ CoreMovies/AddMovieViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddMovieViewController.swift
+//  CoreMovies
+//
+//  Created by Jose on 21/5/19.
+//  Copyright © 2019 appcat.com. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController, UITextFieldDelegate
+{
+
+    override func viewDidLoad()
+	{
+        super.viewDidLoad()
+    }
+	
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool
+	{
+		
+		return true
+	}
+	
+    // MARK: - Navigation
+
+    
+
+}
